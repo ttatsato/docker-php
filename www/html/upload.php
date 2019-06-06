3c6e66c3d310f318eff912e67a2d30fbe2535607
ttatsato <t-satoh@alby.co.jp>
2019-06-06 23:12:53 +0900

    vue ダウンロードしてローカルで使用できるように

--- www/html/upload.php
+++ www/html/upload.php
@@ -1,4 +1,4 @@
 <?php
 // $_POST の値をJSONにして返す簡単なお仕事
 header('Content-Type: application/json; charset=utf-8');
-echo json_encode(['text' => $_POST['text']]);
+echo json_encode(['text' => 'PHPを通した値==>'. $_POST['text']]);
